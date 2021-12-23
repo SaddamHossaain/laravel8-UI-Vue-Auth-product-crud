e38fae8f9aaeabbed11e2f6a740f6aa2f06b8b66
Md Saddam Hossain <77723902+SaddamHossaain@users.noreply.github.com>
2021-12-23 21:00:10 +0600

    laravel vue UI+vue integrate
    1. Setup Database
    2. php artisan make:model Product -m
    3. php artisan migrate
    4. php artisan make:controller Api/ProductController
    5. Create Routes in -> routes/web.php And routes/api.php
    6. Install Laravel Vue UI by cmd:
        ->composer require laravel/ui
    7. php artisan ui vue
    8. npm install
    9. npm install vue-router vue-axios
    10. npm run watch

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,5 +16,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+//api route
+Route::middleware('api')->group(function () {
+    Route::resource('products', ProductController::class);
+});
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//API Route
+Route::get('{any}', function () {
+    return view('layouts.app');
+})->where('any', '.*');
